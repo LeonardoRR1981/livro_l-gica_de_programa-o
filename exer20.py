44d0aeebf9ff65d75a5445674087d2c1d010f5ae
Leonardo <leonardorr81@outlook.com>
2023-10-18 07:56:39 -0300

    new commit

---
+++ exer20.py
@@ -0,0 +1,28 @@
+x = 1
+while x <= 3: 
+    print(x)
+    x += 1
+    
+# Modiﬁque o programa para exibir os números de 1 a 100.
+
+x = 1
+while x <= 100: 
+    print(x)
+    x += 1
+    
+# Modiﬁque o programa para exibir os números de 50 a 100.
+
+x = 50
+while x <= 100: 
+    print(x)
+    x += 1
+    
+#Faça um programa para escrever a contagem regressiva do lançamentode um foguete. 
+# O programa deve imprimir 10, 9, 8, ..., 1, 0 e Fogo! na tela
+
+x = 10
+while x >= 0: 
+    print(x)
+    x -= 1
+print('FOGO!!!')
+    
---
+++ exer21.py
@@ -0,0 +1,14 @@
+# Escreva um programa que leia dois números. Imprima o resultado da multiplicação do primeiro pelo segundo. 
+# Utilize apenas os operadores de soma esubtração para calcular o resultado. 
+# Lembre-se de que podemos entender a multiplicação de dois números como somas sucessivas de um deles.
+#Assim, 4 × 5 = 5+ 5 + 5 + 5 = 4 + 4 + 4 + 4 + 4.
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite um número: '))
+
+x = 1
+r = 0
+while x <= n2:
+    r = r + n1
+    x = x + 1
+print('{} x {} = {}'.format(n1, n2, r))    
